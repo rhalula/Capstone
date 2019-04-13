09f09e931647985a2980b3f3ba6bfc7603ea38c0
Rachel Halula <31323403+rhalula@users.noreply.github.com>
2019-04-13 13:09:03 -0400

    Create about page

---
+++ about.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <title>My test PHP Website</title>
+    </head>
+    <body>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/MyFirstWebsite/styles.css">
+    <div class="container-fluid">
+    	<div class="row">
+    		<div class="col-md-12">
+    			<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-dark bg-dark">
+
+            <a class="navbar-brand" href="#">Learn to Crochet</a>
+    				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    					<ul class="navbar-nav ml-md-auto">
+    						<li class="nav-item active">
+    							 <a class="nav-link" href="login.php">Returning <span class="sr-only">(current)</span></a>
+    						</li>
+                <li class="nav-item active">
+    							 <a class="nav-link" href="register.php">Just Starting <span class="sr-only">(current)</span></a>
+    						</li>
+                <li class="nav-item active">
+    							 <a class="nav-link" href="about.php">About <span class="sr-only">(current)</span></a>
+    						</li>
+                <li class="nav-item active">
+    							 <a class="nav-link" href="faq.php">FAQ <span class="sr-only">(current)</span></a>
+    						</li>
+                <li class="nav-item active">
+    							 <a class="nav-link" href="overview.php">Overview <span class="sr-only">(current)</span></a>
+    						</li>
+                <li class="nav-item active">
+    							 <a class="nav-link" href="#"> <span class="sr-only">(current)</span></a>
+    						</li>
+    					</ul>
+    				</div>
+    			</nav>
+    		</div>
+    	</div>
+    </div>
+    <h2>About</h2>
+    <p>This website was created to merge two of my passions together: Crocheting and Computer Science. WILL ADD MORE, JUST 
+    GETTING EVERYTHING TO WORK RIGHT NOW</p>
+  </body>
+</html>
