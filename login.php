0ec048e0fdbf50862d5a6114199c3678e20fa3e5
Rachel Halula <31323403+rhalula@users.noreply.github.com>
2019-04-13 13:10:03 -0400

    Added navbar

--- login.php
+++ login.php
@@ -3,6 +3,40 @@
         <title>My test PHP Website</title>
     </head>
     <body>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/MyFirstWebsite/styles.css">
+    <div class="container-fluid">
+    	<div class="row">
+    		<div class="col-md-12">
+    			<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-dark bg-dark">
+
+            <a class="navbar-brand" href="#">Learn to Crochet</a>
+    				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    					<ul class="navbar-nav ml-md-auto">
+    						<li class="nav-item active">
+    							 <a class="nav-link" href="login.php">Returning <span class="sr-only">(current)</span></a>
+    						</li>
+                <li class="nav-item active">
+    							 <a class="nav-link" href="register.php">Just Starting <span class="sr-only">(current)</span></a>
+    						</li>
+                <li class="nav-item active">
+    							 <a class="nav-link" href="about.php">About <span class="sr-only">(current)</span></a>
+    						</li>
+                <li class="nav-item active">
+    							 <a class="nav-link" href="faq.php">FAQ <span class="sr-only">(current)</span></a>
+    						</li>
+                <li class="nav-item active">
+    							 <a class="nav-link" href="overview.php">Overview <span class="sr-only">(current)</span></a>
+    						</li>
+                <li class="nav-item active">
+    							 <a class="nav-link" href="#"> <span class="sr-only">(current)</span></a>
+    						</li>
+    					</ul>
+    				</div>
+    			</nav>
+    		</div>
+    	</div>
+    </div>
         <h2>Login Page</h2>
           <a href="index.php">Click here to go back</a><br /><br />
           <form action="checklogin.php" method="POST">
